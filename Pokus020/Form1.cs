e47111b78bc963917c49f3dc4c12a38070380d6f
aodehnalova <aodehnalova@A222-08>
2020-09-24 13:19:14 +0200

    vetev Lojza

--- Pokus020/Form1.cs
+++ Pokus020/Form1.cs
@@ -33,5 +33,10 @@ namespace Pokus020
         {
             //dalsi tlacitko
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //vetev Lojza
+        }
     }
 }
